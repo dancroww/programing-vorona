4ccadad51b02d098c9cf61108daf7d2d917e7369
Vorona_923v <voronadanja@gmail.com>
2023-12-11 12:24:10 +0200

    lab03

--- lab03/src/main.c
+++ lab03/src/main.c
@@ -1,12 +1,9 @@
-#include <stdio.h>		//founnd and learned it when i tried to print sum
 int main()
 {
 	int sum = 0;		//variable for operation's result 
 	int min_num = 40;		//first minimal number
 	int max_num = 45;		// second maximal number
-	for (int i = min_num; i <= max_num; i++) {		//cycle where each next number bigger than previous by 1, starting from minimal number
-		sum = sum + i;		//sum all numbers into one
-	}
-	printf ("\n sum = %d", sum);		//print result
+	int diff = max_num - min_num + 1;	//number of numbers
+	sum = ((max_num + min_num) * diff) / 2; //sum of n-numbers in arifmetic progression
 	return 0;
 }
