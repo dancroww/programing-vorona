aad1db9faa7231c59a318ca434e7b4b8eb438bc5
Vorona_923v <voronadanja@gmail.com>
2023-12-14 22:02:14 +0200

    lab8.1

---
+++ lab8.1/dist/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['nsd_5ffunction_2',['NSD_function',['../main_8c.html#a24430be96f0789e3b95c58dd87332efc',1,'main.c']]],
+  ['num_5fin_5fwords_3',['num_in_words',['../main_8c.html#a42a7271bcf91381fa2c4cd24573af490',1,'main.c']]]
+];
---
+++ lab8.1/dist/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "mnr",
+  1: "mr",
+  2: "mn"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Всі",
+  1: "Файли",
+  2: "Функції"
+};
+
