cfac8ab32575b920820d533c8d7215af32b5a165
Vorona_923v <voronadanja@gmail.com>
2023-12-14 21:59:51 +0200

    lab03

--- lab03/src/main.c
+++ lab03/src/main.c
@@ -1,8 +1,8 @@
 int main()
 {
 	int sum = 0;		//variable for operation's result 
-	int min_num = 40;		//first minimal number
-	int max_num = 45;		// second maximal number
+	int min_num = 100;		//first minimal number
+	int max_num = 12;		// second maximal number
 	int diff = max_num - min_num + 1;	//number of numbers
 	sum = ((max_num + min_num) * diff) / 2; //sum of n-numbers in arifmetic progression
 	return 0;
