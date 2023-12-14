741a4b3d7859ae1171ed3f52308b41394bcb6eef
Vorona_923v <voronadanja@gmail.com>
2023-12-14 21:59:59 +0200

    lab04

--- lab04/src/main.c
+++ lab04/src/main.c
@@ -1,14 +1,20 @@
 int main()
 {
+	float l, s, v;
 	int radius = 40;		//radius variable
 	float pi = 3.14f;		//pi number
 	char command = 'l';
-		if (command == 'l') {		//rozgaluzhennya
-			float l = 2 * pi * radius;
-} else if (command == 's') {
-			float s = pi * (radius * radius);
-} else if (command == 'v') {
-			float v = (4 / 3) * pi * (radius * radius * radius);
+		switch (command)	//rozgaluzhennya
+{
+			case 'l':		
+				l = 2 * pi * radius;
+				break;
+			case 's':
+				s = pi * (radius * radius);
+				break;
+			case 'v':
+				v = (4 / 3) * pi * (radius * radius * radius);
+				break;
 }
 	return 0;
 }
